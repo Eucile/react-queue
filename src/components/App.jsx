07bb42465974478c886fb76122b3529fc3772d39
Krystal Foster <kmfoster777@gmail.com>
2018-11-03 14:45:12 -0700

    link react components

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
+import Ticket from './Ticket';
+
+function App() {
+  return(
+    <div>
+      <Header/>
+      <TicketList/>
+    </div>
+  );
+}
+
+export default App;
