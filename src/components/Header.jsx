315e1e0bd0a47360b61ac4e5846ad2d2b6f1359a
Krystal Foster <kmfoster777@gmail.com>
2018-11-04 11:26:41 -0800

    add basic inline object styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 function Header() {
+  var styledHeader = {
+    backgroundColor: '#ecf0f1',
+    fontFamily: 'sans-serif',
+  }
   return(
-    <h1>Help Queueeeee</h1>
+    <h1 style={styledHeader}>Help Queueeeee</h1>
   );
 }
 
