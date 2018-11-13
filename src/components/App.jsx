c584e06de2679f55a772817fc6c4b49ab090f472
Student <example@example.com>
2018-11-13 10:07:19 -0800

    begin state setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
 import Ticket from './Ticket';
+import NewTicketControl from './NewTicketControl';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return(
     <div>
       <Header/>
-      <TicketList/>
+        <Switch>
+          <Route exact path='/' component={TicketList} />
+          <Route path='/newticket' component={NewTicketControl} />
+        </Switch>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Header() {
-  var styledHeader = {
-    backgroundColor: '#ecf0f1',
-    fontFamily: 'sans-serif',
-  }
-  return(
-    <h1 style={styledHeader}>Help Queueeeee</h1>
+function Header(){
+  return (
+    <div>
+      <h1>Help Queue</h1>
+      <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
+    </div>
   );
 }
 
